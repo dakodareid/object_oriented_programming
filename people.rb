c4c7914b382e86f00e532f8e4801e6a762358907
dakodareid <YOUR EMAIL ADDRESSdakodareid@gmail.com>
2014-10-23 14:53:47 -0400

    part 8 completed

--- people.rb
+++ people.rb
@@ -19,12 +19,16 @@ class Instructor < People
 	end
 end
 
-instructor = Instructor.new
-instructor.name = "Chris"
-instructor.greeting
-instructor.teach
+instructor1 = Instructor.new
+instructor1.name = "Chris"
+instructor1.greeting
+instructor1.teach
 
-student = Student.new
-student.name = "Christina"
-student.greeting
-student.learn
+student1 = Student.new
+student1.name = "Christina"
+student1.greeting
+student1.learn
+
+# Below doesn't work because student1 is part of the Student class and the
+# student class does not have the method 'teach' in its class. 
+student1.teach
