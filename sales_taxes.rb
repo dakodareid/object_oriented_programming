c93a257718ae8e54136965f9344ad136dcfe6b8a
dakodareid <YOUR EMAIL ADDRESSdakodareid@gmail.com>
2014-10-23 22:58:33 -0400

    started bonous assignment

--- sales_taxes.rb
+++ sales_taxes.rb
@@ -8,21 +8,32 @@ class Product
 			@price = price
 		end
 
-		def tax
+		def import_tax
 			(@price * 1.050)
 		end
-
-		
 end
-
-
 
 product1 = Product.new
 product1.name = "chocolate"
 product1.price = 10.00
 
-puts product1.tax 
+puts product1.import_tax 
 puts product1.name
 
+#----------------------------
 
+product2 = Product.new
+product2.name = "novel"
+product2.price = 15.00
 
+puts product2.import_tax 
+puts product2.name
+
+#----------------------------
+
+product3 = Product.new
+product3.name = "bacon"
+product3.price = 11.00
+
+puts product3.import_tax 
+puts product3.name
